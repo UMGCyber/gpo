44bc080bb8ba469bfa7c6d82e5bfad3a7e65d378
Free-Freedy <163952365+Free-Freedy@users.noreply.github.com>
2024-03-19 10:48:46 -0400

    Add files via upload

---
+++ DisableUnnecessaryServices.ps1
@@ -0,0 +1,39 @@
+﻿Get-Service -DisplayName 'Bluetooth Audio Gateway Service' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Bluetooth Support Service' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Downloaded Maps Manager' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Geolocation Service' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Infrared monitor service'  | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Internet Connection Sharing (ICS)'  | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Link-Layer Topology Discovery Mapper' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'LxssManager' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Microsoft iSCSI Initiator Service' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'OpenSSH Authentication Agent' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'OpenSSH SSH Server' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Peer Name Resolution Protocol' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Peer Networking Grouping' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Peer Networking Identity Manager' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'PNRP Machine Name Publication Service' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Print Spooler' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Problem Reports Control Panel Support' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Remote Access Auto Connection Manager' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Remote Desktop Services UserMode Port Redirector' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Remote Procedure Call (RPC) Locator' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Remote Registry' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Routing and Remote Access' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Simple TCP/IP Services' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'SNMP Service' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'SNMP Trap' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Special Administration Console Helper' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'SSDP Discovery' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'UPnP Device Host' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Windows Error Reporting Service' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Windows Event Collector' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Windows Media Player Network Sharing Service' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Windows Mobile Hotspot Service' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Windows Push Notifications System Service' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Windows PushToInstall Service' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Windows Remote Management (WS-Management)' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Xbox Accessory Management Service' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Xbox Live Auth Manager' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Xbox Live Game Save' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
+Get-Service -DisplayName 'Xbox Live Networking Service' | Set-Service -StartupType Disabled | Stop-Service -Force -PassThru -Verbose
